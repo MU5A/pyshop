e8334a248d90b7d98ba21b22e740727743c04801
Musa M0h <bashirmusa748@gmail.com>
2023-03-09 23:21:22 +0100

    added more products features

--- products/models.py
+++ products/models.py
@@ -3,8 +3,12 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=255)
-    price = models.FLoatField()
+    price = models.FloatField()
     stock = models.IntegerField()
     image_url = models.CharField(max_length=2083)
 
-    
+class Offer(models.Model):
+    code = models.CharField(max_length=8)
+    description = models.TextField()
+    discount = models.FloatField()
+
